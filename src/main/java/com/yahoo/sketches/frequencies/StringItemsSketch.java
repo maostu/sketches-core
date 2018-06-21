e4985db69a5217d3f63824b36ce5bd6cbdbc21ed
Manuel Osdoba <manuel.osdoba@tu-ilmenau.de>
2018-06-22 00:24:19 +0200

    Return most frequent items within given error threshold

--- src/main/java/com/yahoo/sketches/frequencies/StringItemsSketch.java
+++ src/main/java/com/yahoo/sketches/frequencies/StringItemsSketch.java
@@ -1,5 +1,7 @@
 package com.yahoo.sketches.frequencies;
 import com.yahoo.sketches.frequencies.ItemsSketch;
+import com.yahoo.sketches.frequencies.ItemsSketch.Row;
+import com.yahoo.sketches.frequencies.ErrorType;
 
 public class StringItemsSketch extends com.yahoo.sketches.frequencies.ItemsSketch<String>
 {
@@ -12,4 +14,10 @@ public class StringItemsSketch extends com.yahoo.sketches.frequencies.ItemsSketc
    {
       super(lgMaxMapSize, lgCurMapSize);
    }
+   
+   public Row<String>[] getFrequentItems(final long threshold) {
+     return sortItems(threshold > getMaximumError() ? threshold : getMaximumError(), m_ErrorType);
+   }
+   
+   protected ErrorType m_ErrorType = ErrorType.NO_FALSE_POSITIVES;
 }
